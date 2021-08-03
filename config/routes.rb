386d25efa8eae7f968c781f221344f93fe751bb9
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-03 20:06:28 +0900

    Add route and migratefile table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'to_do#home'
+
+  get '/new_column', to: 'to_do#new_column'
+  post '/new_column', to: 'to_do#create_column'
+
+  get '/new_card', to: 'to_do#new_card'
+  post '/new_card', to: 'to_do#create_card'
 end
