661675a7745013e1f2b55724dd8c757dc76bad1f
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-03 18:42:12 +0900

    Add controller new and create and Create form

--- app/controllers/to_do_controller.rb
+++ app/controllers/to_do_controller.rb
@@ -1,4 +1,49 @@
 class ToDoController < ApplicationController
 	def home
+		@columns = Column.all
+		@cards = Card.all
 	end
+
+	def new_column
+		@column = Column.new
+	end
+
+	def create_column
+		@column = Column.new(column_params)
+		if @column.save
+			render 'home'
+		else
+			render 'new'
+		end
+	end
+
+	def new_card
+		@card = Card.new
+	end
+
+	def create_card
+		@card = Card.new(card_params)
+		if @card.save
+			render 'home'
+		else
+			render 'new'
+		end
+	end
+
+	private
+		def column_params
+			params.require(:column).permit(
+				:name,
+				:order
+			)
+		end
+
+		def card_params
+			params.require(:card).permit(
+				:name,
+				:discription,
+				:column_id,
+				:deadline
+			)
+		end
 end
