ec0d7ada825024e9e647bdd32c4f662f03502157
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-06 19:26:59 +0900

    Divide column and card html and destroy to_do controller

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,2 +1,24 @@
 class CardController < ApplicationController
+  def new
+    @card = Card.new
+  end
+
+  def create
+		@card = Card.new(card_params)
+		if @card.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+  private
+    def card_params
+      params.require(:card).permit(
+        :name,
+        :discription,
+        :column_id,
+        :deadline
+      )
+    end
 end
