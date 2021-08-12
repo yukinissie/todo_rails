f32e406af4eef21e871b77a520dbf4a9db009a85
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-08-13 01:36:45 +0900

    Add card edit and update controller and Fix route

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -20,6 +20,16 @@ class CardController < ApplicationController
     Card.find(params[:id]).delete
   end
 
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update(card_params)
+    redirect_to columns_path
+  end
+
   private
     def card_params
       params.require(:card).permit(
